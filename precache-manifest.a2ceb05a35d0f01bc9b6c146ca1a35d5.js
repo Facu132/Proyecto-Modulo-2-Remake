e3717998664cc031f8266f7f97bce0c813e78218
Fockxs <facugsil@gmail.com>
2020-08-26 23:26:54 -0300

    Updates

---
+++ precache-manifest.a2ceb05a35d0f01bc9b6c146ca1a35d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9a03f7453271939c1e328af620a90a2",
+    "url": "/Proyecto-Modulo-2-Remake/index.html"
+  },
+  {
+    "revision": "15dc349246adbf6c05b4",
+    "url": "/Proyecto-Modulo-2-Remake/static/css/main.84facf53.chunk.css"
+  },
+  {
+    "revision": "7200cbfb56329afc06f8",
+    "url": "/Proyecto-Modulo-2-Remake/static/js/2.134ef490.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Proyecto-Modulo-2-Remake/static/js/2.134ef490.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15dc349246adbf6c05b4",
+    "url": "/Proyecto-Modulo-2-Remake/static/js/main.a93f1cb4.chunk.js"
+  },
+  {
+    "revision": "b1778383b2df442682b1",
+    "url": "/Proyecto-Modulo-2-Remake/static/js/runtime-main.ffd1e0df.js"
+  }
+]);
